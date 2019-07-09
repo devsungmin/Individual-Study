df87faab2ca17e0e5c3d95c1584d8155aa7d5e86
devsugnmin <kimsungmin.dev@gmail.com>
2019-07-09 22:09:14 +0900

    cuda pooling

---
+++ Cuda/Mpi/pooling/pooling.cu
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <cuda.h>
+#include "mpi.h"
+
+#define ROW 32 //가로
+#define COL 32 //세로
+#define stride 2
+
+MPI_Status status;
+
+__global__ void MaxPooling(float* dev_a,float* dev_b, float* dev_c, int arr_size,int start_range, int end_range) {
+	int i = threadIdx.x;
+	int j = blockIdx.x;
+
+	if(start_range <= j && j < end_range) {
+		for(int x=0; x<arr_size; x++) {
+			dev_c[arr_size * j + i] += dev_a[arr_size * j + x] * dev_b[arr_size * x + i];
+		}
+	}	
+}
+
+int main(int argc, char **argv) {
+	int arr_size = ROW*COL; // 32 * 32 = 1024	
+	/*행렬 선언 및 초기화*/
+	float*  a[arr_size]; 
+	float*  b[arr_size];
+	float*  c[arr_size];
+	/*gpu 행렬*/
+	float* dev_a[arr_size];
+	float* dev_b[arr_size];
+	float* dev_c[arr_size];
+
+	int rank,size; //mpi사용을 위한 변수 설정
+	int offset=0;
+	int before_offset=0;
+	
+	size_t bytes = arr_size * arr_size * sizeof(float);
+	
+	/*MPI 환경 초기화*/
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	
+	a = (float*)malloc(bytes);
+	b = (float*)malloc(bytes);
+	c = (float*)malloc(bytes);
+
+	for(int i=0;i<arr_size; i++) {
+		/*0 ~ 1023까지 정적으로 고정 되어 들어감*/
+		a[i] = static_cast<float>;
+		b[i] = static_cast<float>;
+		c[i] = 0;
+	}
+
+	cudaMalloc((void**)&dev_a, bytes);
+	cudaMalloc((void**)&dev_b, bytes);
+	cudaMalloc((void**)&dev_c, bytes);
+
+	cudaStatus = cudaDeviceSynchronize();
+	if(cudaStatus != cudaSuccess) {
+		fprintf("cudaDeviceSynchronize returned error code %d after launching a kernel!!\n", cudaStatus);
+		return -1;
+	}
+
+	if(rank == 0) {
+		//MPI_Recv
+	}
+	else if(rank > 0) {
+		//MPI_Send
+	}
+
+	free(a);
+	free(b);
+	free(c);
+
+	cudaFree(dev_a);
+	cudaFree(dev_b);
+	cudaFree(dev_c);
+	MPI_Finalze(); //MPI환경 초기화
+	return 0;
+}
