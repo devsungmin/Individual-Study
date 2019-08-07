21d442a274c0378b7f6eedcbafaedc2f51232546
devsugnmin <kimsungmin.dev@gmail.com>
2019-08-07 19:33:11 +0900

    update cuda pooling layer

--- Cuda/Mpi/pooling/Cuda_Avg_pooling.cu
+++ Cuda/Mpi/pooling/Cuda_Avg_pooling.cu
@@ -8,76 +8,59 @@
 
 using namespace std;
 
-void Init_input(float* input, int input_channel, int input_h_size, int input_w_size)
+void Init_input(float* input, int input_h_size, int input_w_size)
 {
    srand(time(NULL));
 
-   for (int c = 0; c < input_channel; c++)
-   {
       for (int h = 0; h < input_h_size; h++)
       {
          for (int w = 0; w < input_w_size; w++)
          {
-            input[(c * input_h_size * input_w_size) + (h * input_w_size) + w] = rand() % 10;
+            input[(h * input_w_size) + w] = rand() % 10;
          }
       }
-   }
 }
 
-__global__ void Avg_pooling(int input_channel, int pooled_h, int pooled_w, int pool_h_stride, int pool_w_stride, int pool_h_size, int input_h_size, int pool_w_size, int input_w_size, int sum, float avg, float *input, float *gpu_output_data, int rank, int size)
+__global__ void Avg_pooling(int pooled_h, int pooled_w, int pool_h_stride, int pool_w_stride, int pool_h_size, int input_h_size, int pool_w_size, int input_w_size, int sum, float avg, float* gpu_input, float* gpu_output_data)
 {
-  int i = blockIdx.x;
-  int j = blockIdx.x;
+   int i = blockIdx.x;
+   int j = blockIdx.y;
+   int h_start = j * pool_h_stride;
+   int w_start = i * pool_w_stride;
+   int h_end = min(h_start + pool_h_size, input_h_size);
+   int w_end = min(w_start + pool_w_size, input_w_size);
+             
+   h_start = max(h_start, 0);
+   w_start = max(w_start, 0);
+             
+   sum=0;
+   avg=0.0;
 
-  for (int c = 0; c < input_channel; c++)
+   int pool_index = (j * pooled_w) + i;
+   for (int h = h_start; h < h_end; h++)
    {
-      for (int ph = 0; ph < pooled_h; ph++)
+      for (int w = w_start; w < w_end; w++)
       {
-         for (int pw = 0; pw < pooled_w; pw++)
-         {
-             int h_start = ph * pool_h_stride;
-             int w_start = pw * pool_w_stride;
-             int h_end = min(h_start + pool_h_size, input_h_size);
-             int w_end = min(w_start + pool_w_size, input_w_size);
-             
-             h_start = max(h_start, 0);
-             w_start = max(w_start, 0);
-             
-             sum=0;
-             avg=0.0;
-
-            int pool_index = (c * pooled_h * pooled_w) + (ph * pooled_w) + pw;
-            for (int h = h_start; h < h_end; h++)
-            {
-               for (int w = w_start; w < w_end; w++)
-               {
-                  int index = (c * input_h_size * input_w_size) + (h * input_w_size) + w;
-                  sum += input[index];
-	       }
-            avg = (float)sum / (pool_h_size * pool_w_size);
-
-            gpu_output_data[pool_index] = avg;
-           }
-        }
-      }
+         int index = (h * input_w_size) + w;
+         sum += gpu_input[index];
+	   }
+      avg = (float)sum / (pool_h_size * pool_w_size);
+      gpu_output_data[pool_index] = avg;
    }
 }
 
-void print(float* data, int ch, int h_size, int w_size)
+void print(float* data, int h_size, int w_size)
 {
-   for (int c = 0; c < ch; c++)
-   {
       for (int h = 0; h < h_size; h++)
       {
          for (int w = 0; w < w_size; w++)
          {
-            printf("%.2f  ", data[(c * h_size * w_size) + (h * w_size) + w]);
+            printf("%.2f  ", data[(h * w_size) + w]);
          }
          cout << endl;
       }
       cout << endl;
       cout << endl;
-   }
 }
 
 int main()
@@ -87,8 +70,6 @@ int main()
 
    int input_h_size = 6;
    int input_w_size = 6;
-   int input_channel = 1;
-
    /*pool => window size*/
    int pool_w_size = 3;
    int pool_h_size = 3;
@@ -99,31 +80,35 @@ int main()
    int pooled_h = ((input_h_size - pool_h_size) / pool_h_stride) + 1;
    int pooled_w = ((input_w_size - pool_w_size) / pool_w_stride) + 1;
 
-   float* input = new float[input_channel * input_h_size * input_w_size];
+   float* input = new float[input_h_size * input_w_size];
+   float* cpu_output_data = new float[input_h_size * input_w_size];
 
+   Init_input(input, input_h_size, input_w_size);
+   print(input, input_h_size, input_w_size);
 
-   Init_input(input, input_channel, input_h_size, input_w_size);
-   print(input, input_channel, input_h_size, input_w_size);
+   float* gpu_input;
+   float* gpu_output_data; 
 
-   float* gpu_output_data = new float[input_channel * input_h_size * input_w_size];
+   cudaMalloc((void**)&gpu_input, input_h_size*input_w_size);
+   cudaMalloc((void**)&gpu_output_data, input_h_size*input_w_size);
 
-   cudaMalloc((void**)&input, input_channel*input_h_size*input_w_size);
-   cudaMalloc((void**)&gpu_output_data, input_channel*input_h_size*input_w_size);
+   cudaMemcpy(gpu_output_data, input, input_h_size*input_w_size, cudaMemcpyHostToDevice);
 
-   cudaMemcpy(gpu_output_data,input,input_channel*input_h_size*input_w_size, cudaMemcpyHostToDevice);
-
-   dim3 grid(input_h_size, input_w_size);
-
-   Avg_pooling<<< input_h_size, input_w_size  >>>(input_channel, pooled_h, pooled_w, pool_h_stride, pool_w_stride, pool_h_size, input_h_size, pool_w_size, input_w_size, sum, avg, input, gpu_output_data, rank, size);
-   cudaDeviceSynchronize();
-
-   print(gpu_output_data, input_channel, pooled_h, pooled_w);
+   dim3 dimGrid(input_h_size, input_w_size);
+   dim3 dimBlock(1, 1);
    
-   cudaFree(input);
+   Avg_pooling<<< dimGrid, dimBlock >>>(pooled_h, pooled_w, pool_h_stride, pool_w_stride, pool_h_size, input_h_size, pool_w_size, input_w_size, sum, avg, gpu_input, gpu_output_data);
+   
+   cudaMemcpy(cpu_output_data, gpu_output_data, input_h_size*input_w_size, cudaMemcpyDeviceToHost);
+   //cudaDeviceSynchronize();
+
+   print(cpu_output_data, pooled_h, pooled_w);
+   
+   cudaFree(gpu_input);
    cudaFree(gpu_output_data);
 
    delete input;
-   delete gpu_output_data;
+   delete cpu_output_data;
 
    return 0;
 }
