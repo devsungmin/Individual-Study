4e3c97b7e30fe4aa99f5790ce5ac7e62557cab39
devsugnmin <kimsungmin.dev@gmail.com>
2019-08-12 01:39:45 +0900

    update MPI Cuda Pooling

--- Cuda/Mpi/pooling/MPI_Cuda_Avg_pooling.cu
+++ Cuda/Mpi/pooling/MPI_Cuda_Avg_pooling.cu
@@ -9,19 +9,19 @@
 using namespace std;
 MPI_Status status;
 
-void Init_input(float* input, int input_h_size, int input_w_size)
+void Init_input(float* cpu_input, int input_h_size, int input_w_size)
 {
    srand(time(NULL));
    for (int h = 0; h < input_h_size; h++)
    {
       for (int w = 0; w < input_w_size; w++)
       {
-         input[(h * input_w_size) + w] = rand() % 10;
+         cpu_input[(h * input_w_size) + w] = rand() % 10;
       }
    }
 }
 
-__global__ void Avg_pooling(int pooled_h, int pooled_w, int pool_h_stride, int pool_w_stride, int pool_h_size, int input_h_size, int pool_w_size, int input_w_size, int sum, float avg, float* gpu_input, float* gpu_output_data)
+__global__ void Avg_pooling(int start_range, int end_range, int pooled_h, int pooled_w, int pool_h_stride, int pool_w_stride, int pool_h_size, int input_h_size, int pool_w_size, int input_w_size, int sum, float avg, float* gpu_input, float* gpu_output_data)
 {
    int i = blockIdx.x;
    int j = blockIdx.y;
@@ -37,15 +37,19 @@ __global__ void Avg_pooling(int pooled_h, int pooled_w, int pool_h_stride, int p
    avg=0;
 
    int pool_index = (j * pooled_w) + i;
-   for (int h = h_start; h < h_end; h++)
+
+   if(start_range<=j && j<end_range)
    {
-      for (int w = w_start; w < w_end; w++)
+      for (int h = h_start; h < h_end; h++)
       {
-         int index = (h * input_w_size) + w;
-         sum += gpu_input[index];
-	   }
-      avg = (float)sum / (pool_h_size * pool_w_size);
-      gpu_output_data[pool_index] = avg;
+         for (int w = w_start; w < w_end; w++)
+         {
+            int index = (h * input_w_size) + w;
+            sum += gpu_input[index];
+	      }
+         avg = (float)sum / (pool_h_size * pool_w_size);
+         gpu_output_data[pool_index] = avg;
+      }
    }
 }
 
@@ -76,22 +80,25 @@ int main(int argc, char** argv)
    int pool_w_stride = 3;
    int pool_h_stride = 3;
 
-   int size;
-   int rank;
+   int size, rank;
 
    /*pooling 된 행렬들*/
    int pooled_h = ((input_h_size - pool_h_size) / pool_h_stride) + 1;
    int pooled_w = ((input_w_size - pool_w_size) / pool_w_stride) + 1;
 
-   float* input = new float[input_h_size * input_w_size * sizeof(float)];
+   float* cpu_input = new float[input_h_size * input_w_size * sizeof(float)];
    float* cpu_output_data = new float[input_h_size * input_w_size* sizeof(float)];
 
    MPI_Init(&argc, &argv);
    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
    MPI_Comm_size(MPI_COMM_WORLD, &size);
+   Init_input(cpu_input, input_h_size, input_w_size);
 
-   Init_input(input, input_h_size, input_w_size);
-   print(input, input_h_size, input_w_size);
+   if(rank == 0)
+   {
+      printf("\t====행렬 초기 값====\n");
+      print(cpu_input, input_h_size, input_w_size);
+   }
 
    float* gpu_input;
    float* gpu_output_data; 
@@ -99,46 +106,67 @@ int main(int argc, char** argv)
    cudaMalloc((void**)&gpu_input, input_h_size*input_w_size* sizeof(float));
    cudaMalloc((void**)&gpu_output_data, input_h_size*input_w_size* sizeof(float));
 
-   //cudaMemcpy(gpu_input, input, input_h_size*input_w_size* sizeof(float), cudaMemcpyHostToDevice);	
-
    dim3 dimGrid(input_h_size, input_w_size);
    dim3 dimBlock(1, 1);
-   
+
    if(rank == 0)
    {
       int start_range = ((input_h_size*input_w_size)/size)*rank;
       int end_range = ((rank+1)*((input_h_size*input_w_size)/size));
 
-      for(int i=1; i<size; i++)
+      for(int i=1; i<size/2; i++)
       {
-         MPI_Send(input, input_h_size * input_w_size, MPI_FLOAT, i, 1, MPI_COMM_WORLD);
+         MPI_Send(cpu_input, input_h_size * input_w_size, MPI_FLOAT, i, 1, MPI_COMM_WORLD);
       }
-      cudaMemcpy(gpu_input, input, input_h_size*input_w_size* sizeof(float), cudaMemcpyHostToDevice);	
-      Avg_pooling<<< dimGrid, dimBlock >>>(pooled_h, pooled_w, pool_h_stride, pool_w_stride, pool_h_size, input_h_size, pool_w_size, input_w_size, sum, avg, gpu_input, gpu_output_data);
+      cudaMemcpy(gpu_input, cpu_input, input_h_size*input_w_size* sizeof(float), cudaMemcpyHostToDevice);
+
+      Avg_pooling<<< dimGrid, dimBlock >>>(start_range, end_range, pooled_h, pooled_w, pool_h_stride, pool_w_stride, pool_h_size, input_h_size, pool_w_size, input_w_size, sum, avg, gpu_input, gpu_output_data);
+      
+      cudaDeviceSynchronize();
       cudaMemcpy(cpu_output_data, gpu_output_data, input_h_size*input_w_size* sizeof(float), cudaMemcpyDeviceToHost);
+       printf("======\n\n");
+       print(cpu_output_data,pooled_h,pooled_w);
+       printf("======\n\n");
+      for(int i=1; i<size/2; i++)
+      {
+         MPI_Recv(cpu_input, input_h_size * input_w_size, MPI_FLOAT, i, 1, MPI_COMM_WORLD, &status);
+      }
    }
-   else if(rank > 0)
-  {
-   int start_range = ((input_h_size*input_w_size)/size)*rank;
-   int end_range = ((rank+1)*((input_h_size*input_w_size)/size));
+    else if(rank > 0)
+    {
+       int start_range = ((input_h_size*input_w_size)/size)*rank;
+       int end_range = ((rank+1)*((input_h_size*input_w_size)/size));
+       for(int i=size/2; i<size; i++)
+       {
+          MPI_Recv(cpu_input, input_h_size * input_w_size, MPI_FLOAT, i, 1, MPI_COMM_WORLD, &status);
+       }
+       
+       cudaMemcpy(gpu_input, cpu_input, input_h_size*input_w_size* sizeof(float), cudaMemcpyHostToDevice);
 
-    MPI_Recv(input, input_h_size * input_w_size, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, &status);
+       Avg_pooling<<< dimGrid, dimBlock >>>(start_range, end_range, pooled_h, pooled_w, pool_h_stride, pool_w_stride, pool_h_size, input_h_size, pool_w_size, input_w_size, sum, avg, gpu_input, gpu_output_data);
+       
+       cudaDeviceSynchronize();
+       cudaMemcpy(cpu_output_data, gpu_output_data, input_h_size*input_w_size* sizeof(float), cudaMemcpyDeviceToHost);
 
-    cudaMemcpy(gpu_input, input, input_h_size*input_w_size* sizeof(float), cudaMemcpyHostToDevice);	
-    Avg_pooling<<< dimGrid, dimBlock >>>(pooled_h, pooled_w, pool_h_stride, pool_w_stride, pool_h_size, input_h_size, pool_w_size, input_w_size, sum, avg, gpu_input, gpu_output_data);
-    cudaMemcpy(cpu_output_data, gpu_output_data, input_h_size*input_w_size* sizeof(float), cudaMemcpyDeviceToHost); 
-  }
-   
-   //cudaDeviceSynchronize();
+       printf("======\n\n");
+       print(cpu_output_data,pooled_h,pooled_w);
+       printf("======\n\n");
 
-   //printf("====GPU Pooling Result value=====\n");
+       for(int i=size/2; i<size; i++)
+       {
+         MPI_Send(cpu_input, input_h_size * input_w_size, MPI_FLOAT, i, 1, MPI_COMM_WORLD);
+       }
+   }
+   cudaDeviceSynchronize();
+
+   printf("===MPI_Pooling된 행렬 값===\n");
    print(cpu_output_data, pooled_h, pooled_w);
 
    
    cudaFree(gpu_input);
    cudaFree(gpu_output_data);
 
-   delete input;
+   delete cpu_input;
    delete cpu_output_data;
 
    return 0;
